3d8526816635bb8db58341370ce7f24c90c187c1
Zanus <francesco.zanusfortes@gmail.com>
2021-02-06 20:20:04 +0100

    Working device selection and playback functions

--- GUI.py
+++ GUI.py
@@ -1,12 +1,23 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 
 
 class MyLayout(BoxLayout):
-    pass
-    # def cacca(self):
-    #     self.ids
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        playback : DropDown = self.ids['playback']
+        btn = Button(text='Ciao ciao', size_hint_y=None, height='48dp', on_release = playback.select)
+        # Button:
+        # text: 'Value A'
+        # size_hint_y: None
+        # height: '48dp'
+        # on_release: playback.select('A')
+        playback.add_widget(btn)
+
+
 
 class GUI(App):
     def build(self):
--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-from GUI import *
+import time
+from numpy import array_split
+import soundcard as sc
+from audio2numpy import open_audio
+import pyaudio, ffmpeg, wave
 
 if __name__ == '__main__':
-    GUI().run()
+    # GUI().run()
+
+    p = pyaudio.PyAudio()
+    # info = p.get_host_api_info_by_index(0)
+    # numdevices = info.get('deviceCount')
+    # # for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
+    # for i in range(0, numdevices):
+    #     if p.get_device_info_by_host_api_device_index(0, i).get('maxOutputChannels') > 0:
+    #         print("Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name'))
+
+    # sp = sc.get_speaker('{0.0.0.00000000}.{7a17482b-40aa-497f-a8e2-f36a90a58861}')
+    #
+    # signal, sampling_rate = open_audio(r'C:\Users\Winblu\Desktop\ts sound\avemo capito non ce ne frega un cazzo cicciogamer89.mp3')
+    #
+
+    # devinfo = p.get_device_info_by_index(12)
+    # print("\n\nSelected device is " + devinfo['name'])
+    # if p.is_format_supported(48000.0,  # Sample rate
+    #                          output_device=devinfo["index"],
+    #                          output_channels=devinfo['maxOutputChannels'],
+    #                          output_format=pyaudio.paInt16):
+    #     print('Yay!')
+    chunk = 1024
+    
+    wf = wave.open('audio.wav', 'rb')
+
+    stream = p.open(format= p.get_format_from_width(wf.getsampwidth()),
+           channels= wf.getnchannels(),
+           rate = wf.getframerate(),
+           output= True, output_device_index= 12)
+
+    data = wf.readframes(chunk)
+
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    stream.close()
+    p.terminate()
